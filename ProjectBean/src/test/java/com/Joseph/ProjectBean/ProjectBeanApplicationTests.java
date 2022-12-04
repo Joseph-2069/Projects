433f52b65e80b4211efdcd58da55f0cb378debcd
Joseph Vibin Verghese <joseph.vibin1999@gmail.com>
2022-12-04 21:25:25 +0530

    Commit

---
+++ ProjectBean/src/test/java/com/Joseph/ProjectBean/ProjectBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Joseph.ProjectBean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
